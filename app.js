4135afaed274335ae26631357a653730692f76b7
Mausp7 <archie.tombacz@gmail.com>
2022-02-14 22:02:55 +0100

    Return dummy json data over api/dummy.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const fs = require("fs")
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +13,16 @@ app.get('/', (req, res) => {
     res.send('<h1>Server is ON</h1>')
 });
 
+app.get('/api/dummy', (req, res) => {
+    fs.readFile('data/dummy.json', 'utf8', (err, data) => {
+        if (err) {
+            res.json(err)
+            return
+        }
+        const DB = JSON.parse(data);
+        res.json(DB);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`)
