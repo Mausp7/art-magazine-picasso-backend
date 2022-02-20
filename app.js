e828d6401bab98489bc0d0cf5358482543d13e84
Mausp7 <archie.tombacz@gmail.com>
2022-02-20 12:48:10 +0100

    Adds user signup endpoint.

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const port = 5000;
 
 
 app.get('/', (req, res) => {
-    res.send('<h1>Server is ON</h1>')
+    res.send('<h1>Picasso API Node Server is listening...</h1>')
 });
 
-app.get('/api/users', (req, res) => {
+app.get('/api/users', (req, res) => { // Endpoint returning all users.
     fs.readFile('data/users.json', 'utf8', (err, data) => {
         if (err) {
             res.json(err)
@@ -24,7 +24,32 @@ app.get('/api/users', (req, res) => {
     });
 });
 
-app.get('/api/user/:id', (req, res) => {
+app.post('/api/user/signup', (req, res) => { // Endpoint to register a new user.
+    fs.readFile('data/users.json', 'utf8', (err, data) => {
+        if (err) res.sendStatus(500);
+
+        if (!req.body.username || !req.body.password) {
+            return res.sendStatus(400);
+        };
+
+        const DB = JSON.parse(data);
+        const user = DB.find(user => user.username.toLowerCase() === req.body.username.toLowerCase())
+        if (user) return res.sendStatus(409);
+
+        const newUser = {
+            id: DB.reduce((max, user) => user.id > max ? user.id : max, 0) + 1,
+            username: req.body.username,
+            password: req.body.password,
+            collection: []
+        };
+
+        DB.push(newUser);
+        fs.writeFileSync('./data/users.json', JSON.stringify(DB, null, 4), 'utf8', (err, data) => {})
+        return res.json(newUser);
+    });
+});
+
+app.get('/api/user/:id', (req, res) => { // Endpint to return a specific user.
     fs.readFile('data/users.json', 'utf8', (err, data) => {
         if (err) res.sendStatus(500);
 
@@ -39,7 +64,23 @@ app.get('/api/user/:id', (req, res) => {
     });
 });
 
-app.post('/api/user/:id', (req, res) => {
+app.delete('/api/users/:id', (req, res) => { // Endpoint to delete a specific user.
+    fs.readFile('data/users.json', 'utf8', (err, data) => {
+        if (err) res.sendStatus(500);
+
+        const userID = Number(req.params.id);
+        if (!userID || userID < 1) return res.sendStatus(400);
+        
+        let DB = JSON.parse(data);
+        if (!DB.find(user => user.id === userID)) return res.sendStatus(404);
+
+        DB = DB.filter(user => user.id !== userID)
+        fs.writeFileSync('data/users.json', JSON.stringify(DB, null, 4),'utf8')
+        return res.sendStatus(200);
+    });
+});
+
+app.post('/api/user/:id', (req, res) => { // Endpoint posting an artpiece into a specific users collection.
     fs.readFile('data/users.json', 'utf8', (err, data) => {
         if (err) res.sendStatus(500);
 
@@ -70,7 +111,7 @@ app.post('/api/user/:id', (req, res) => {
     });
 });
 
-app.put('/api/user/:id', (req, res) => {
+app.put('/api/user/:id', (req, res) => { //Endpoint to update a specific users specific artpiece.
     fs.readFile('data/users.json', 'utf8', (err, data) => {
         if (err) res.sendStatus(500);
 
@@ -100,7 +141,7 @@ app.put('/api/user/:id', (req, res) => {
     });
 });
 
-app.delete('/api/user/:id', (req, res) => {
+app.delete('/api/user/:id', (req, res) => { // Endpoint to delete a specific users artpiece by url or title.
     fs.readFile('data/users.json', 'utf8', (err, data) => {
         if (err) res.sendStatus(500);
 
@@ -116,22 +157,6 @@ app.delete('/api/user/:id', (req, res) => {
         user.collection = user.collection.filter(art => art.url !== req.query.url && art.title !== req.query.title);
         fs.writeFileSync('data/users.json', JSON.stringify(DB, null, 4),'utf8')
         return res.json(user.collection);
-    });
-});
-
-app.delete('/api/users/:id', (req, res) => {
-    fs.readFile('data/users.json', 'utf8', (err, data) => {
-        if (err) res.sendStatus(500);
-
-        const userID = Number(req.params.id);
-        if (!userID || userID < 1) return res.sendStatus(400);
-        
-        let DB = JSON.parse(data);
-        if (!DB.find(user => user.id === userID)) return res.sendStatus(404);
-
-        DB = DB.filter(user => user.id !== userID)
-        fs.writeFileSync('data/users.json', JSON.stringify(DB, null, 4),'utf8')
-        return res.sendStatus(200);
     });
 });
 
