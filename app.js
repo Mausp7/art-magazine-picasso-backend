430343ef82c482b83f9f43b878ddebb9e088123f
Mausp7 <archie.tombacz@gmail.com>
2022-02-21 23:24:52 +0100

    Authentication added for applicable endpoints.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 const port = 5000;
-const activeSessions = {};
+const activeSessions = {"12345": {username: "JohnDoe", password: "f4hgv6beD5rHTf4h6FGfg4 "}};
 
 
 app.get('/', (req, res) => {
@@ -63,7 +63,7 @@ app.get('/api/user/login', (req, res) => { // Endpoint to validate an existing u
         if (err) res.sendStatus(500);
 
         const DB = JSON.parse(data);
-        const user = DB.find((user) => user.username === authHeader.username && user.password === authHeader.password);
+        const user = DB.find(user => user.username === authHeader.username && user.password === authHeader.password);
         if (!user) return res.sendStatus(401);
 
         const sessionId = Math.floor(Math.random() * 10000000).toString();
@@ -76,51 +76,63 @@ app.get('/api/user/login', (req, res) => { // Endpoint to validate an existing u
     });
 });
 
-app.get('/api/user/:id', (req, res) => { // Endpoint to get user collection.
-    const userID = Number(req.params.id);
-    if (!userID || userID < 1) return res.sendStatus(400);
+app.delete('/api/user/delete', (req, res) => { // Endpoint to delete a specific user.
+    if (!req.header("Authorization")) return res.sendStatus(400);
 
-    fs.readFile('data/users.json', 'utf8', (err, data) => {
-        if (err) res.sendStatus(500);
+    const sessionId = JSON.parse(req.header("Authorization"));
+    if (!activeSessions[sessionId]) return res.sendStatus(401);
 
-        const DB = JSON.parse(data);
-        const user = DB.find((user) => user.id === userID);
-        if (!user) return res.sendStatus(401);
-
-        return res.json(user);
-    });
-});
-
-app.delete('/api/users/:id', (req, res) => { // Endpoint to delete a specific user.
-    const userID = Number(req.params.id);
-    if (!userID || userID < 1) return res.sendStatus(400);
+    const username = activeSessions[sessionId].username;
 
     fs.readFile('data/users.json', 'utf8', (err, data) => {
         if (err) res.sendStatus(500);
 
         let DB = JSON.parse(data);
-        if (!DB.find(user => user.id === userID)) return res.sendStatus(404);
+        if (!DB.find(user => user.username === username)) return res.sendStatus(404);
 
-        DB = DB.filter(user => user.id !== userID)
-        fs.writeFileSync('data/users.json', JSON.stringify(DB, null, 4),'utf8')
+        DB = DB.filter(user => user.username !== username);
+        fs.writeFileSync('data/users.json', JSON.stringify(DB, null, 4),'utf8');
         return res.sendStatus(200);
     });
 });
 
-app.post('/api/user/:id', (req, res) => { // Endpoint posting an artpiece into a specific users collection.
-    const userID = Number(req.params.id);
-    if (!userID || userID < 1) return res.sendStatus(400);
+app.get('/api/user/collection', (req, res) => { // Endpoint to get user collection.
+    if (!req.header("Authorization")) return res.sendStatus(400);
+
+    const sessionId = JSON.parse(req.header("Authorization"));
+    if (!activeSessions[sessionId]) return res.sendStatus(401);
+
+    const username = activeSessions[sessionId].username;
 
     fs.readFile('data/users.json', 'utf8', (err, data) => {
         if (err) res.sendStatus(500);
 
         const DB = JSON.parse(data);
-        const user = DB.find(user => user.id === userID)
+        const user = DB.find(user => user.username === username);
+        if (!user) return res.sendStatus(404);
+
+        return res.json(user.collection);
+    });
+});
+
+app.post('/api/user/collection', (req, res) => { // Endpoint posting an artpiece into a specific users collection.
+    if (!req.header("Authorization")) return res.sendStatus(400);
+
+    const sessionId = JSON.parse(req.header("Authorization"));
+    if (!activeSessions[sessionId]) return res.sendStatus(401);
+
+    const username = activeSessions[sessionId].username;
+
+    fs.readFile('data/users.json', 'utf8', (err, data) => {
+        if (err) res.sendStatus(500);
+
+        const DB = JSON.parse(data);
+        const user = DB.find((user) => user.username === username);
         if (!user) return res.sendStatus(404);
 
         if (!req.body.artist || !req.body.title || !req.body.url) return res.sendStatus(400);
 
-        if (user.collection.find((art) => art.title === req.body.title) || user.collection.find((art) => art.url === req.body.url)) return res.sendStatus(409);
+        if (user.collection.find(art => art.title === req.body.title) || user.collection.find(art => art.url === req.body.url)) return res.sendStatus(409);
 
         saveTempImage(req.body.url, `./temp/temp`);
 
@@ -134,51 +146,58 @@ app.post('/api/user/:id', (req, res) => { // Endpoint posting an artpiece into a
         };
 
         user.collection.push(newCollection);
-        fs.writeFileSync('data/users.json', JSON.stringify(DB, null, 4),'utf8')
-
+        fs.writeFileSync('data/users.json', JSON.stringify(DB, null, 4),'utf8');
         return res.json(user.collection);
     });
 });
 
-app.put('/api/user/:id', (req, res) => { //Endpoint to update a specific users specific artpiece.
-    const userID = Number(req.params.id);
-    if (!userID || userID < 1) return res.sendStatus(400);
-
+app.put('/api/user/collection', (req, res) => { //Endpoint to update a specific users specific artpiece.
     if (!req.body.artist || !req.body.title || !req.body.url) return res.sendStatus(400);
+
+    if (!req.header("Authorization")) return res.sendStatus(400);
+    
+    const sessionId = JSON.parse(req.header("Authorization"));
+    if (!activeSessions[sessionId]) return res.sendStatus(401);
+
+    const username = activeSessions[sessionId].username;
 
     fs.readFile('data/users.json', 'utf8', (err, data) => {
         if (err) res.sendStatus(500);
 
         const DB = JSON.parse(data);
-        const user = DB.find(user => user.id === userID)
+        const user = DB.find((user) => user.username === username);
         if (!user) return res.sendStatus(404);
 
-        const art = user.collection.find((art) => art.title === req.body.title) || user.collection.find((art) => art.url === req.body.url);
+        const art = user.collection.find(art => art.title === req.body.title) || user.collection.find(art => art.url === req.body.url);
 
         art.tags= req.body.tags ? req.body.tags : [];
         art.rating = Number(req.body.rating) < 6 && Number(req.body.rating) > -1  ? Number(req.body.rating) : 0;
         art.description = req.body.description ? req.body.description : "";
 
-        fs.writeFileSync('data/users.json', JSON.stringify(DB, null, 4),'utf8')
+        fs.writeFileSync('data/users.json', JSON.stringify(DB, null, 4),'utf8');
         return res.json(user.collection);
     });
 });
 
-app.delete('/api/user/:id', (req, res) => { // Endpoint to delete a specific users artpiece by url or title.
+app.delete('/api/user/collection', (req, res) => { // Endpoint to delete a specific users artpiece by url or title.
     if (!req.query.url && !req.query.title) return res.sendStatus(400);
 
-    const userID = Number(req.params.id);
-    if (!userID || userID < 1) return res.sendStatus(400);
+    if (!req.header("Authorization")) return res.sendStatus(400);
+
+    const sessionId = JSON.parse(req.header("Authorization"));
+    if (!activeSessions[sessionId]) return res.sendStatus(401);
+
+    const username = activeSessions[sessionId].username;
 
     fs.readFile('data/users.json', 'utf8', (err, data) => {
         if (err) res.sendStatus(500);
 
         const DB = JSON.parse(data);
-        const user = DB.find(user => user.id === userID)
+        const user = DB.find(user => user.username === username);
         if (!user) return res.sendStatus(404);
 
         user.collection = user.collection.filter(art => art.url !== req.query.url && art.title !== req.query.title);
-        fs.writeFileSync('data/users.json', JSON.stringify(DB, null, 4),'utf8')
+        fs.writeFileSync('data/users.json', JSON.stringify(DB, null, 4),'utf8');
         return res.json(user.collection);
     });
 });
